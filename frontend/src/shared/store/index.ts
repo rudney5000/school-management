25027c5a270a413fdd9a629ebe87f274c2a9d012
rudney5000 <rudneyngueko@gmail.com>
2026-05-29 05:40:00 +0300

    feat: added auth slice in slice

--- frontend/src/shared/store/index.ts
+++ frontend/src/shared/store/index.ts
@@ -1,8 +1,11 @@
 import {configureStore} from "@reduxjs/toolkit";
 import {setupListeners} from "@reduxjs/toolkit/query";
+import {authSlice} from "@features/auth/store/auth-slice.ts";
 
 export const store = configureStore({
-    reducer: {},
+    reducer: {
+        auth: authSlice.reducer,
+    },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
 })
 
