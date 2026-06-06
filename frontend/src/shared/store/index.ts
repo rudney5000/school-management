99bcb57a6ae030bf6e0fef43a7fc4c336b44922f
rudney5000 <rudneyngueko@gmail.com>
2026-06-06 03:58:56 +0300

    feat: connect all slice in global store

--- frontend/src/shared/store/index.ts
+++ frontend/src/shared/store/index.ts
@@ -2,11 +2,23 @@ import {configureStore} from "@reduxjs/toolkit";
 import {setupListeners} from "@reduxjs/toolkit/query";
 import {authSlice} from "@features/auth/store/auth-slice";
 import {countrySlice} from "@entities/country";
+import {citySlice} from "@entities/city";
+import {districtSlice} from "@entities/district";
+import {departmentSlice} from "@entities/department";
+import {schoolSlice} from "@entities/school";
+import {studentSlice} from "@entities/student";
+import {subSchoolSlice} from "@entities/sub-school/model/slice";
 
 export const store = configureStore({
     reducer: {
         auth: authSlice.reducer,
         country: countrySlice.reducer,
+        city: citySlice.reducer,
+        district: districtSlice.reducer,
+        department: departmentSlice.reducer,
+        school: schoolSlice.reducer,
+        subSchool: subSchoolSlice.reducer,
+        student: studentSlice.reducer,
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
 })
