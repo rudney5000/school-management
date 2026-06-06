453cbcab6503bfa88de7ee1584f4c2389c83e079
rudney5000 <rudneyngueko@gmail.com>
2026-06-06 18:13:42 +0300

    feat: next task

--- frontend/src/app/i18n/i18n.ts
+++ frontend/src/app/i18n/i18n.ts
@@ -6,6 +6,26 @@ import en from '@/shared/config/locales/en.json'
 import ru from '@/shared/config/locales/ru.json'
 import ln from  '@/shared/config/locales/ln.json'
 
+// Next task
+// import frCommon from '@shared/config/locales/fr/common.json'
+// import frStudents from '@shared/config/locales/fr/dashboard/students.json'
+// import enCommon from '@shared/config/locales/en/common.json'
+// import enStudents from '@shared/config/locales/en/dashboard/students.json'
+
+// i18n.init({
+//     resources: {
+//         fr: {
+//             common: frCommon,
+//             students: frStudents,
+//         },
+//         en: {
+//             common: enCommon,
+//             students: enStudents,
+//         },
+//     },
+//     defaultNS: 'common',
+//     fallbackLng: 'fr',
+// })
 i18n
     .use(initReactI18next)
     .init({
