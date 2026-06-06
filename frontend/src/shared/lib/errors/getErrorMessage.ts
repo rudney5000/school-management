bc6165c1c1aad02ee14b9e42888bc0aba0c1aa6e
rudney5000 <rudneyngueko@gmail.com>
2026-06-06 03:41:21 +0300

    refactoring: new logic of get error message function

--- frontend/src/shared/lib/errors/getErrorMessage.ts
+++ frontend/src/shared/lib/errors/getErrorMessage.ts
@@ -1,14 +1,19 @@
 import type {Locale} from "@shared/config/i18n/locale-config";
 import {errors} from "@shared/lib";
 import type {ErrorKey} from "@shared/lib";
-
-const currentLang: Locale = "fr";
-
+import i18n from "@app/i18n/i18n";
 export function getErrorMessage(path: ErrorKey): string {
     const [group, key] = path.split(".") as [
         keyof typeof errors,
         string
     ];
 
-    return errors[group][key as keyof typeof errors[typeof group]][currentLang];
+    const lang = i18n.language as Locale
+
+    return (
+        errors[group]?.[
+            key as keyof typeof errors[typeof group]
+            ]?.[lang] ??
+            path
+    );
 }
