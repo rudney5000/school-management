ff2d654b4c296d1d7787205495ff6623f921c664
rudney5000 <rudneyngueko@gmail.com>
2026-06-06 03:40:16 +0300

    feat: new custom drawer ui component

---
+++ frontend/src/shared/ui/custom-drawer/custom-drawer.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import {
+    Sheet,
+    SheetContent, SheetDescription,
+    SheetHeader,
+    SheetTitle,
+} from "@shared/ui"
+
+type CustomDrawerProps = {
+    drawerTitle?: string;
+    drawerDescription?: string
+    handleOpen?: (e: any) => void;
+    isOpen?: boolean
+    children?: React.ReactNode
+    direction?: "top" | "bottom" | "left" | "right"
+}
+const CustomDrawer: React.FC<CustomDrawerProps> = ({ drawerTitle, drawerDescription, children, isOpen, handleOpen, direction }) => {
+    return (
+        <Sheet open={isOpen} onOpenChange={handleOpen}>
+            <SheetContent className="z-50 bg-white border-0" side={direction}>
+                <SheetHeader>
+                    <SheetTitle>{drawerTitle}</SheetTitle>
+                    <SheetDescription>
+                        {drawerDescription}
+                    </SheetDescription>
+                </SheetHeader>
+                {children}
+            </SheetContent>
+        </Sheet>
+
+    )
+}
+
+export default CustomDrawer
