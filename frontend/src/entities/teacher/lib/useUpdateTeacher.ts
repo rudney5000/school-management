ea15108bf3dc512d05b58dc801ff48c909c0c93d
rudney5000 <rudneyngueko@gmail.com>
2026-06-09 01:16:18 +0300

    fix: entity teacher

--- frontend/src/entities/teacher/lib/useUpdateTeacher.ts
+++ frontend/src/entities/teacher/lib/useUpdateTeacher.ts
@@ -1,15 +1,15 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { handleApiError } from '@shared/lib'
-import {studentApi, type UpdateStudentDto} from "@entities/student";
+import {teacherApi, type UpdateTeacherDto} from "@entities/teacher";
 
 export const useUpdateTeacher = () => {
     const queryClient = useQueryClient()
 
     return useMutation({
-        mutationFn: ({ id, dto, subSchoolId }: { id: string; dto: UpdateStudentDto, subSchoolId: string }) =>
-            studentApi.update(id, dto, subSchoolId),
+        mutationFn: ({ id, dto, subSchoolId }: { id: string; dto: UpdateTeacherDto, subSchoolId: string }) =>
+            teacherApi.update(id, dto, subSchoolId),
         onSuccess: () => {
-            queryClient.invalidateQueries({ queryKey: ['students'] })
+            queryClient.invalidateQueries({ queryKey: ['teachers'] })
         },
         onError: (error: Error) => {
             handleApiError(error)
