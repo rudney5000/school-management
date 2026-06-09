4731df54448a0cfe3c3fe631f2d66b7025eec1a4
rudney5000 <rudneyngueko@gmail.com>
2026-06-10 02:13:59 +0300

    feat: added preview action

--- frontend/src/shared/ui/common/ActionsComponent.tsx
+++ frontend/src/shared/ui/common/ActionsComponent.tsx
@@ -3,16 +3,18 @@ import { Icons } from '../icons';
 import { ColumnActions, DropdownMenuItem } from "@shared/ui";
 import type { Row } from "@tanstack/react-table";
 import { useTranslation } from "@shared/lib";
+import {Eye} from "lucide-react";
 
 interface ActionsComponentProps<TData> {
     row: Row<TData>,
     children?: React.ReactNode,
     onDeleteAction: (original: TData) => void,
     onEditAction: (original: TData) => void
+    onViewAction?: (original: TData) => void
 }
 
 export function ActionsComponent<TData>(props: ActionsComponentProps<TData>) {
-    const { row, children, onEditAction, onDeleteAction } = props;
+    const { row, children, onEditAction, onDeleteAction, onViewAction } = props;
     const { t } = useTranslation()
 
     return (
@@ -33,6 +35,15 @@ export function ActionsComponent<TData>(props: ActionsComponentProps<TData>) {
                     <Icons.trash2 className="h-4 w-4" />
                     <p>{t('dashboard.common.actions.delete')}</p>
                 </DropdownMenuItem>
+                {onViewAction && (
+                    <DropdownMenuItem
+                        className="space-x-4"
+                        onClick={() => onViewAction(row.original)}
+                    >
+                        <Eye className="h-4 w-4" />
+                        <p>{t('dashboard.common.actions.view')}</p>
+                    </DropdownMenuItem>
+                )}
             </ColumnActions>
         </div>
     )
