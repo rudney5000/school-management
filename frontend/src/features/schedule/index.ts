1389b78b03c5f034f0d66b953997eb6b823a4bfc
rudney5000 <rudneyngueko@gmail.com>
2026-06-18 23:27:23 +0300

    fix: schedule feat

--- frontend/src/features/schedule/index.ts
+++ frontend/src/features/schedule/index.ts
@@ -1,6 +1,5 @@
 export * from './ui/AddScheduleForm'
 export * from './ui/EditScheduleForm'
 export * from './ui/DeleteScheduleAlert'
-export * from './ui/ScheduleOverviewPanel'
 export * from './ui/ScheduleCalendar'
 export * from './ui/ScheduleList'
